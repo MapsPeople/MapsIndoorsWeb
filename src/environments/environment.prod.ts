ae5d5137c9fc9344dfba10504332c94f60374468
Simon Laustsen <sl@mapspeople.com>
2019-12-12 13:33:45 +0000

    Release 3.7.12

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	version: '3.7.10',
+	version: '3.7.12',
 	v: require('../../package.json').version,
 	sdkUrl: 'https://app.mapsindoors.com/mapsindoors/js/sdk/3.4.1/mapsindoors-3.4.1.js.gz',
 	suggestedSolutionId: 'demo'
