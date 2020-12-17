b7899374c646651a298dbe2821a8d12c3adfb2f1
Simon Laustsen <sl@mapspeople.com>
2020-12-17 08:08:48 +0000

    Release 4.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    version: '3.13.0',
+    version: '4.0.0',
     v: require('../../package.json').version,
     sdkUrl: 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.1.3/mapsindoors-4.1.3.js.gz',
     sentryDsn: '',
