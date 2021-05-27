3a8fcd73ae1e51938fc9fc1a330baa9b100eb4bb
Simon Laustsen <sl@mapspeople.com>
2021-05-27 06:20:59 +0000

    Release 5.8.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    version: '5.8.0',
+    version: '5.8.1',
     v: require('../../package.json').version,
-    sdkUrl: 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.8.1/mapsindoors-4.8.1.js.gz',
+    sdkUrl: 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.10.0/mapsindoors-4.10.0.js.gz',
     sentryDsn: '',
     suggestedSolutionId: 'demo'
 };
